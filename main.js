9d4219b6b239e8dc36af4cc38b5c3159e85cf3e8
Anton Khomenko <anton.khomenko@yahoo.com>
2019-02-14 21:04:13 +0200

    Fix array gen

--- main.js
+++ main.js
@@ -12,20 +12,29 @@ genButt.addEventListener("click", function(){
     if(checkVar == 0){
         let t = document.createElement("table");
         let tr = document.createElement("tr");
+        var td = document.createElement("td");
+        var inpt = document.createElement("input");
     // t.style.border = "1px solid #fff";
         t.style.cellpadding = "5";
     // td.style.width = "50px";
     // td.style.height = "50px";
         t.appendChild(tr);
+        td.appendChild(inpt);
+        tr.appendChild(td);
         iab[0].appendChild(t);
+        inpt.classList.add("inptStyle");
+        td.style.height = "50px";
+        td.style.width = "50px";
+        td.style.border = "1px solid #fff";
     for(let i = 0; i < inputArraySizeInpt.value; i++){
-        let td = document.createElement("td");
-        let inpt = document.createElement("input");
+        var td = document.createElement("td");
+        var inpt = document.createElement("input");
         td.appendChild(inpt);
         tr.appendChild(td);
         td.style.height = "50px";
         td.style.width = "50px";
         td.style.border = "1px solid #fff";
+        td.style.after
         inpt.classList.add("inptStyle");
         }
     }
