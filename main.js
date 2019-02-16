ebe0fc970fe27e0a311ebb46a5536fc92b4849b4
Anton Khomenko <anton.khomenko@yahoo.com>
2019-02-16 16:05:30 +0200

    loop wich create js array from visual interpritation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var inputArraySizeInpt = document.getElementById("array-size-inpt");
 var valueRes = document.getElementById("value-res");
 var genButt = document.getElementById("genButt");
 var iab = document.getElementsByClassName("input-Array-block");
+var sortBtn = document.getElementById("sort-btn");
 valueRes.textContent = inputArraySizeInpt.value;
 
 inputArraySizeInpt.addEventListener("input", function(){
@@ -12,20 +13,12 @@ genButt.addEventListener("click", function(){
     if(checkVar == 0){
         let t = document.createElement("table");
         let tr = document.createElement("tr");
-        var td = document.createElement("td");
-        var inpt = document.createElement("input");
     // t.style.border = "1px solid #fff";
         t.style.cellpadding = "5";
     // td.style.width = "50px";
     // td.style.height = "50px";
         t.appendChild(tr);
-        td.appendChild(inpt);
-        tr.appendChild(td);
         iab[0].appendChild(t);
-        inpt.classList.add("inptStyle");
-        td.style.height = "50px";
-        td.style.width = "50px";
-        td.style.border = "1px solid #fff";
     for(let i = 0; i < inputArraySizeInpt.value; i++){
         var td = document.createElement("td");
         var inpt = document.createElement("input");
@@ -34,9 +27,20 @@ genButt.addEventListener("click", function(){
         td.style.height = "50px";
         td.style.width = "50px";
         td.style.border = "1px solid #fff";
-        td.style.after
         inpt.classList.add("inptStyle");
         }
     }
    checkVar = 1;
 })
+
+sortBtn.addEventListener("click", function(){
+    if(checkVar == 1){
+        let tdA = iab[0].querySelectorAll("td");
+        var jsArray = [];
+        jsArray.lenght = tdA.length;
+        for(let i = 0; i < tdA.length; i++){
+            jsArray[i] = tdA[i].children[0].value;
+        };
+        console.log(jsArray);
+    };
+});
